ecd906288f6d4395d70dfdd648a8cc30425adf58
MARI2020201101 <mari20201101@gmail.com>
2021-11-26 11:21:40 +0900

    Add HelloServiceV3

--- src/main/java/com/mari/spring/app/v3/HelloServiceV3.java
+++ src/main/java/com/mari/spring/app/v3/HelloServiceV3.java
@@ -1,8 +1,10 @@
 package com.mari.spring.app.v3;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class HelloServiceV3 {
 
     public void hello(){
