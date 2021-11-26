673c2a64f8aa98e86d69e6270f9dda4cd6fe8e8e
MARI2020201101 <mari20201101@gmail.com>
2021-11-26 11:18:09 +0900

    Add MemberServiceImplV4

--- src/main/java/com/mari/spring/app/v4/MemberServiceImpl.java
+++ src/main/java/com/mari/spring/app/v4/MemberServiceImpl.java
@@ -1,2 +1,22 @@
-package com.mari.spring.app.v4;public class MemberServiceImpl {
+package com.mari.spring.app.v4;
+
+import com.mari.spring.app.v4.annotation.MyClassAnnotation;
+import com.mari.spring.app.v4.annotation.MyMethodAnnotation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@MyClassAnnotation
+@Slf4j
+public class MemberServiceImpl implements MemberService{
+    @Override
+    @MyMethodAnnotation("super super cute")
+    public String hello(String message) {
+        return "hello " + message;
+    }
+
+    @MyMethodAnnotation("super super cute")
+    public void hello2(String message){
+        log.info("hello {} ", message);
+    }
 }
